3ea12ed77a7830013aaf665440f8e79c2aafadd8
Edy kurniawan <edy.kurniawan280898@gmail.com>
2023-08-22 16:50:02 +0700

    default struktur laravel

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// import controller
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\Admin\ArtikelController;
-use App\Http\Controllers\User\HomeController as UserHomeController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,39 +17,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route untuk view sesi-1
-Route::get('/sesi-1', function () {
-    $nama = 'Rizky Syaiful Ramadhan';
-    
-    return view('sesi-1', [
-        'nama'      => $nama,
-        'nim'       => '1941720013',
-        'kelas'     => 'TI-2D'
-    ]);
-});
-
-// Route untuk view belajar blade
-Route::get('/belajar-blade/{nama}', function ($nama) {
-    return view('belajar', [
-        'nama'      => $nama
-    ]);
-});
-
-// Route untuk admin
-Route::get('/admin', function () {
-    return view('admin.index');
-});
-
-// route untuk Home Controller
-Route::get('/sesi-3', [HomeController::class, 'index']);
-Route::get('/belajar', [HomeController::class, 'belajar']);
-Route::get('/belajar2', [HomeController::class, 'belajar2']);
-
-// route untuk Dashboard Controller
-Route::resource('dashboard', DashboardController::class);
-
-// route untuk Artikel Controller
-Route::resource('artikel', ArtikelController::class);
-
-// route untuk User Home Controller
-Route::resource('home', UserHomeController::class);
