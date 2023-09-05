dbf67e057c1568b028d4c452d1093487d6389cff
Edy kurniawan <edy.kurniawan280898@gmail.com>
2023-09-05 15:37:27 +0700

    sesi 1

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\Admin\ArtikelController;
-use App\Http\Controllers\User\HomeController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,23 +16,4 @@ use App\Http\Controllers\User\HomeController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// route untuk view sesi 2
-Route::get('/sesi-2', function () {
-    // mereturn view dan mengirimkan data
-    return view('sesi-2', [
-        'nama_siswa' => 'Edy Kurniawan',
-        'umur' => 19,
-        'mata_pelajaran' => ['Matematika', 'Fisika', 'Kimia', 'Biologi']
-    ]);
-});
-
-// route untuk dashboard controller
-Route::resource('dashboard', DashboardController::class);
-
-// route untuk admin, Artikel Controller
-Route::resource('artikel', ArtikelController::class);
-
-// route untuk user, Home Controller
-Route::resource('home', HomeController::class);
 
